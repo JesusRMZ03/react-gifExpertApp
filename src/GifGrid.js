7dc3473025187bbfc93efbbbe1e77657a31d2567
javier <javier@dojodevs.io>
2021-01-14 21:16:19 -0600

    Testing PRs

--- src/GifGrid.js
+++ src/GifGrid.js
@@ -1,12 +1,11 @@
-import React from 'react' //{ useState, useEffect }
-import { useFetchGifs } from './hooks/useFetchGifs'
+import React from 'react'; //{ useState, useEffect }
+import { useFetchGifs } from './hooks/useFetchGifs';
 import { GifGridItem } from './components/GifGridItem';
 // import { getGifs } from './helpers/getGifs';
 
 export const GifGrid = ({category})=>{
-
-    // const [images, setimages] = useState([]);
-    const {data:images,loading}= useFetchGifs(category);
+	// const [images, setimages] = useState([]);
+	const {data:images,loading}= useFetchGifs(category);
 
 
     // useEffect( ()=> {
@@ -17,24 +16,23 @@ export const GifGrid = ({category})=>{
 
     return(
     <>
-        <h3 className = 'animate__animated animate__backInDown'>{category}</h3>
-        {/* Es el titulo que aparece tras buscar */}
-        {loading && <p className= 'animate__animated animate__flash'>Loading</p>}
-        {
-            !loading && (
-                <div className="card-grid">
-
-                        {
-                            images.map( img=> (
-                                <GifGridItem
-                                key={img.id}
-                                {...img}
-                                />
-                            ))
-                        }
-                </div>
-            )
-        }
+			<h3 className = 'animate__animated animate__backInDown'>{category}</h3>
+			{/* Es el titulo que aparece tras buscar */}
+			{loading && <p className= 'animate__animated animate__flash'>Loading</p>}
+			{
+				!loading && (
+					<div className="card-grid">
+					{
+						images.map( img => (
+							<GifGridItem
+								key={img.id}
+								{...img}
+							/>
+						))
+											}
+							</div>
+					)
+			}
     </>
-    )
-}
+  );
+};
